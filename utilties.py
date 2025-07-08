a63a6041bfed990b06f98a466fa26da716cfe4c2
AlhikamWarsawa <alhikam200@gmail.com>
2025-07-08 10:35:59 +0700

    replikasi

--- utilties.py
+++ utilties.py
@@ -1,5 +1,19 @@
 import copy
-import torch.nn as nn
+import torch.nn as neural
 
 def replikasi(block, N: int = 6):
-    block_stack = nn.ModuleList([copy.deepcopy(block) for _ in range(N)])
+    block_stack = neural.ModuleList([copy.deepcopy(block) for _ in range(N)])
+    return block_stack
+
+if __name__ == "__main__":
+    class EncoderBlock(neural.Module):
+        def __init__(self):
+            super(EncoderBlock, self).__init__()
+            self.layer = neural.Linear(10, 10)
+
+        def forward(self, x):
+            return self.layer(x)
+
+    encoder_block = EncoderBlock()
+    encoder_stack = replikasi(encoder_block, N=6)
+    print(encoder_stack)
