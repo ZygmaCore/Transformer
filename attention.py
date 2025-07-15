bfa1e64d22362595d22a666af702f8f3459ad0ed
AlhikamWarsawa <alhikam200@gmail.com>
2025-07-15 20:56:49 +0700

    encoder decoder

--- attention.py
+++ attention.py
@@ -16,3 +16,42 @@ class MultiHeadAttention(nn.Module):
         self.value = nn.Linear(self.head, self.head, bias=False)
 
         self.fc_output = nn.Linear(self.head * self.heads, dimensi_embedding)
+
+    def forward(self, key: torch.Tensor, query: torch.Tensor, value: torch.Tensor, mask: torch.Tensor = None) -> torch.Tensor:
+        batch_size = key.size(0)
+        k_len, q_len, v_len = key.size(1), query.size(1), value.size(1)
+
+        key = key.reshape(batch_size, k_len, self.heads, self.head)
+        query = query.reshape(batch_size, q_len, self.heads, self.head)
+        value = value.reshape(batch_size, v_len, self.heads, self.head)
+
+        key = self.key(key)
+        query = self.query(query)
+        value = self.value(value)
+
+        product = torch.einsum("bqhd,bkhd->bhqk", [query, key])
+
+        if mask is not None:
+            product = product.masked_fill(mask == 0, float(-1e20))
+
+        product = product / sqrt(self.head)
+        score = F.softmax(product, dim = -1)
+
+        output = torch.einsum("bhqv,bvhd->bqhd", [score, value]). reshape(batch_size, q_len, self.heads * self.head)
+
+        output = self.fc_output(output)
+        return output
+
+if __name__ == '__main__':
+    dimensi_embedding = 512
+    heads = 8
+    attention_layer = MultiHeadAttention(dimensi_embedding, heads)
+
+    batch_size = 32
+    seq_len = 10
+    key = torch.rand([batch_size, seq_len, dimensi_embedding])
+    query = torch.rand([batch_size, seq_len, dimensi_embedding])
+    value = torch.rand([batch_size, seq_len, dimensi_embedding])
+
+    output = attention_layer(key, query, value)
+    print(output.shape)
---
+++ decoder.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+from torch.nn.functional import dropout
+
+from utilties import replikasi
+from attention import MultiHeadAttention
+from embedding import PositionalEncoding
+from encoder import TransformerBlock
+
+class DecoderBlock(nn.Module):
+    def __init__(self, dimensi_embedding: int = 512, heads: int = 8, faktor_ekspansi: int = 4, dropout: float = 0.2) -> None:
+        super(DecoderBlock, self).__init__()
+        self.attention = MultiHeadAttention(dimensi_embedding, heads)
+        self.norm = nn.LayerNorm(dimensi_embedding)
+        self.dropout = nn.Dropout(dropout)
+        self.transformerBlock = TransformerBlock(dimensi_embedding, heads, faktor_ekspansi, dropout)
+
+    def forward(self, key: torch.Tensor, query: torch.Tensor, x: torch.Tensor, mask: torch.Tensor = None) -> torch.Tensor:
+        decoder_attention = self.attention(x, x, x mask)
+        value = self.dropout(self.norm(decoder_attention + x))
+        decoder_attention_output = self.transformerBlock(key, query, value)
+        return decoder_attention_output
+
+class Decoder(nn.Module):
+    def __init__(self, ukuran_target_vocab: int, panjang_sekuens: int, dimensi_embedding: int = 512, jumlah_blocks: int = 6, faktor ekspansi: int = 4, heads: int = 8, dropout: float = 0.2) -> None:
+        super(Decoder, self).__init__()
